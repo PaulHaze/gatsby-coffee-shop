b87bd913b33509c481a406ebef3ebcad6117b117
PaulHaze <20569604+PaulHaze@users.noreply.github.com>
2021-02-17 12:31:25 +1100

    re added layout module

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import styles from "./Layout.module.css"
 export default function Layout({ children }) {
   return (
     <div>
-      <header id={styles.header}>
+      <header id={styles.header} className="">
         <div id={styles.inner}>
           <h1>Joe&apos;s Coffee Shop</h1>
         </div>
